5472d007b2ce41c1065ac6f8b63441960a27a23a
dnshshkr <danish.shukor@live.iium.edu.my>
2023-01-26 09:08:58 +0800

    modified

--- ISR.ino
+++ ISR.ino
@@ -1,11 +1,10 @@
 /********************Interrupt function*******************************************************************************/
 void countPulse()
 {
+  prevMillisEngOff = millis();
   fuelPulsePeriod = float((millis() - measPlsPreviousTime)) / 1000;
   measuredPulsePerMin = 1.0 / ((1.0 / flowRateBias) * fuelPulsePeriod * (1.0 / 60.0));
   measPlsPreviousTime = millis();
-  prevMillisEngOff = millis();
-  //pulseCounter++;
   totalFuelPulse += 1;
   pulse_fuelToWaterRatioCount++;
   if (pulse_fuelToWaterRatioCount >= pulse_fuelToWaterRatio + solenoidOnPulse + 1)
