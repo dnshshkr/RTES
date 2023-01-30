6277b4f7071f8959ce827a67d10c3d29fea85db0
dnshshkr <danish.shukor@live.iium.edu.my>
2023-01-30 13:51:13 +0800

    modified

--- memorySettings.ino
+++ memorySettings.ino
@@ -37,33 +37,16 @@ void saveSettings()
 void loadSettings()
 {
   EEPROM.get(addr1, emulsionTrig);
-  //Serial.print("A: Emulsion State: "); Serial.println(read1);
-
   EEPROM.get(addr2, pulse_fuelToWaterRatio);
-  //Serial.print("B: Pulse Count Max: "); Serial.println(read2);
-
   EEPROM.get(addr3, engineOffTimeOut);
-  //Serial.print("C: Pulse time for idle in ms: "); Serial.println(read3);
-
-  EEPROM.get(addr4, flowRateBias);  //Float
-  //Serial.print("D: Flow Rate Bias in mm/pulse: ");  Serial.println(read4,2);
-
-  EEPROM.get(addr5, solShotBias);  //Float
-  //Serial.print("E: Solenoid Shot Bias in mm/pulse: "); Serial.println(read5,2);
-
+  EEPROM.get(addr4, flowRateBias);
+  EEPROM.get(addr5, solShotBias);
   EEPROM.get(addr6, solenoidOnTime);
-  //Serial.print("F: Solenoid On Time ms: "); Serial.println(read6);
-
   EEPROM.get(addr7, manualPumpState);
-  //Serial.print("G: Manual Mode State: "); Serial.println(read7);
   if (manualPumpState)
     settingMode = 1;
-
   EEPROM.get(addr8, fuelTrig);
-  //Serial.print("G: Manual Mode State: "); Serial.println(read7);
-
   EEPROM.get(addr9, waterTrig);
-
   EEPROM.get(addr10, quickWaterPercentage);
   calculatePulse_fuelToWaterRatio();
   calculateSolenoidOnTime();
