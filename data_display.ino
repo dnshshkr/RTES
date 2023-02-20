f58bf730660523c8bf17141337677caef1adb194
dnshshkr <danish.shukor@live.iium.edu.my>
2023-02-20 15:51:39 +0800

    modified

--- data_display.ino
+++ data_display.ino
@@ -15,16 +15,16 @@ void printData() {
 
   //if(bt)
   //{
-  bt.print(String(totalFuelPulse) + " total fuel pulse \t");
-  bt.print(String(fuelPulsePeriod) + " s\t");
+  bt.print(String(totalFuelPulse) + ",");
+  bt.print(String(totalWaterPulse) + ",");
+  bt.print(String(fuelPulsePeriod) + ",");
   bt.print(measuredPulsePerMin, 3);
-  bt.print(" mL/min\t");
-  bt.print(String(f2wPulseRatioCount) + " counter\t");
-  bt.print(String(totalWaterPulse) + " total water pulse\t");
+  bt.print(",");
+  bt.print(String(f2wPulseRatioCount) + ",");
   bt.print(waterPercentage, 1);
-  bt.print(" % water\t");
+  bt.print(",");
   bt.print(fuelPercentage, 1);
-  bt.println("% fuel");
+  bt.println();
   //}
   pulseDataPrint = false;  //change to false after printing the data and wait for next fuel pulse
 }
