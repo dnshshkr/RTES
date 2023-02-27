8837fc69f76fa38af998611fe365fce534738fb0
dnshshkr <danish.shukor@live.iium.edu.my>
2023-02-27 08:43:49 +0800

    modified

--- master-slave/master/parseSlave.ino
+++ master-slave/master/parseSlave.ino
@@ -1,34 +1,34 @@
-//void parseSlave()
-//{
-//  uint8_t respCode = slave.read();
-//  if (respCode == 0xfa)
-//  {
-//    mode = false;
-//    printSettings();
-//    Serial.println("RTES mode entered");
-//  }
-//  else if (respCode == 0xfb)
-//  {
-//    mode = true;
-//    printSettings();
-//    Serial.println("Settings mode entered");
-//  }
-//  else if (respCode >= 0xfe)
-//  {
-//    String body = slave.readStringUntil('\r\n');
-//    body.trim();
-//    //    Serial.println("response code: " + String(respCode));
-//    //    Serial.println("body: " + body);
-//    if (respCode == 0xfe)
-//    {
-//      readings = JSON.parse(body);
-//      displayData();
-//    }
-//    else if (respCode == 0xff)
-//    {
-//      params = JSON.parse(body);
-//      assignParams();
-//      printSettings();
-//    }
-//  }
-//}
+void parseSlave()
+{
+  uint8_t respCode = slave.read();
+  if (respCode == 0xfa)
+  {
+    mode = false;
+    printSettings();
+    Serial.println("RTES mode entered");
+  }
+  else if (respCode == 0xfb)
+  {
+    mode = true;
+    printSettings();
+    Serial.println("Settings mode entered");
+  }
+  else if (respCode >= 0xfe)
+  {
+    String body = slave.readStringUntil('\r\n');
+    body.trim();
+    //    Serial.println("response code: " + String(respCode));
+    //    Serial.println("body: " + body);
+    if (respCode == 0xfe)
+    {
+      readings = JSON.parse(body);
+      displayData();
+    }
+    else if (respCode == 0xff)
+    {
+      params = JSON.parse(body);
+      assignParams();
+      printSettings();
+    }
+  }
+}
